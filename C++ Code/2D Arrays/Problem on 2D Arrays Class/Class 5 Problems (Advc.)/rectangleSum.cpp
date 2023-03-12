3c588c724bd856cc33ee6cefe7abf429c1d23a78
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-03-12 14:38:27 +0530

    commit

---
+++ C++ Code/2D Arrays/Problem on 2D Arrays Class/Class 5 Problems (Advc.)/rectangleSum.cpp
@@ -0,0 +1,19 @@
+// Problem: Find the sum of all the elements in a rectangle in a 2D array
+// This problem is based on the "Prefix Sum" concept
+
+/*
+Given a matrix 'a' of dimensions nxm and 2 coordinates (l1, r1) and (l2, r2). Return the sum of the recatngle formed under these coordinates
+*/
+
+#include <bits/stdc++.h> // HEADER FILE
+
+using namespace std;
+
+int main() // MAIN DEFINATION
+{
+    ios_base::sync_with_stdio(false); // For fast I/O
+    cin.tie(0);
+    cout.tie(0);
+
+    return 0;
+}
