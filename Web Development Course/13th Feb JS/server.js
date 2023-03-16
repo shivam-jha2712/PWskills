1dfe0c5100f376b07bba0684d9cc7e42e7e6ab61
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-03-16 19:04:59 +0530

    commit

--- Web Development Course/13th Feb JS/server.js
+++ Web Development Course/13th Feb JS/server.js
@@ -1,3 +1,33 @@
 // Hello World in Nodejs
 
 console.log("Hello I am Shivam Jha");
+
+// Primitive data type
+
+// - number: 2,3,4,5, 4.5, 54.75489
+// - string: "Shivam" or 'Shivam'
+// - Boolean: true, false 
+// - null: This is empty data type and has no value assigned to it
+// - undefined: it is given when a variable is unintialized
+
+// Non - Primitive Data Type
+
+// - Array: [] - Same as list in python (thus can store data of various data types in general)
+// Example: 
+let names = ["Shivam", "Jha", "Hitesh", "Ram"]
+console.log(names)
+
+// (Also it is important to note that js is very forgiving thus if you forget to put ";" there would be no such error)
+
+// - Object: {} - This is very important but would be discussed later
+
+// KEYWORDS in js
+// a) let : This is used to store a variable data
+
+let data = "Shivam Jha"
+data = "Hitesh"
+console.log(data)
+
+// b) const : This is used to  store a constant data
+const naam = "Shivam Jha"
+console.log(naam)
