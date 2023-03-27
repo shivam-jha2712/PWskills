f1b497449bebe9007f7602c7c1e65cbac542fcf8
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-03-27 13:53:13 +0530

    commit

--- C++ Code/L. Pointers/1.cpp
+++ C++ Code/L. Pointers/1.cpp
@@ -7,12 +7,6 @@ int main() // MAIN DEFINATION
     ios_base::sync_with_stdio(false);  //For fast I/O
     cin.tie(0);
     cout.tie(0);
-    int t;
-    cout << "JAI SHREE RAM Number: " << endl;
-    cin >> t;
-    while(t--)
-    {
-        cout << t << endl;
-    }
+
     return 0;
 }
