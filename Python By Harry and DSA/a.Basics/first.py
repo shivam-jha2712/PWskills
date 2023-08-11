3a20dbed44dcfaf4389c5fed728f2bc8ff4cc790
Shivam Jha <71783722+shivam-jha2712@users.noreply.github.com>
2023-08-11 15:55:56 +0530

    commit

---
+++ Python By Harry and DSA/a.Basics/first.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+# Comment in Python
+'''This is a Multiline comment in Python'''
+
+# Escape Sequence in Python
+print("Hey there\n And I know you are having a great day.")
+
+# print statement other things
+# print(object, sep = "seprator", end = "end", file=file, flush=flush)
+print("Hey", "Agent", 1, 2, 3, sep="~", end="007\n")
+print("Shivam")
